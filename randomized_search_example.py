848c9624552e2baabd897263ff2f858cd3f45a0d
pablo-fdz <pablo.fdzalb@gmail.com>
2024-12-12 12:04:15 +0100

    Created examples of pipeline use cases

---
+++ randomized_search_example.py
@@ -0,0 +1,46 @@
+import ml_library as ml
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV, cross_validate
+from scipy.stats import uniform
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+
+# Data
+train_data = pd.DataFrame(data = {
+    'A': [1, 5, 6, 8, 10, 10, 15, 16, 7, 8],
+    'B': [0, 3, 50, 2000, 2, 76, 62, 23, 1, 56],
+    'target': [1, 0, 0, 0, 1, 0, 1, 1, 0, 1]
+})
+
+X_train = train_data[['A', 'B']]
+y_train = train_data['target']
+
+test_data = pd.DataFrame(data = {
+    'A': [8, 6, 5, 3, 8, 23, 5, 6, 7, 8],
+    'B': [8, 5, 59, 2333, 1, 123, 45, 23, 7, 19]
+})
+
+X_test = test_data[['A', 'B']]
+
+# Create preprocessing pipeline
+nan_remover = ml.NanRemover()
+standardizer = ml.Standardizer()
+pipeline = ml.PreprocessingPipeline([nan_remover, standardizer])
+
+# Create model
+logreg = LogisticRegression(max_iter = 200,
+                            penalty = 'l2',
+                            fit_intercept = True)
+
+# Create pipeline
+pipeline = ml.MyPipeline(model = logreg, preprocessing = pipeline, score_metric = accuracy_score)
+
+distributions = {
+            'model__C': uniform(loc=0.01, scale=10),
+            }
+
+clf = RandomizedSearchCV(pipeline, distributions, n_iter = 10, random_state=0)
+
+search = clf.fit(X_train, y_train)
+
+print(search.best_params_)
