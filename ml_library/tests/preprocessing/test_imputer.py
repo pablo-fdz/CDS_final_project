705d0e4f507a7e96d2e85e0fe58a5e44efdfce1d
blancajimenezzzz <blanca.jimenezvicente@gmail.com>
2024-12-13 12:48:14 +0100

    minor changes

--- ml_library/tests/preprocessing/test_imputer.py
+++ ml_library/tests/preprocessing/test_imputer.py
@@ -6,17 +6,17 @@ from preprocessing.transformations.imputer import Imputer
 class TestImputer(unittest.TestCase):
     
     def setUp(self):
-        # Sample DataFrame
+        # SAMPLE DATAFRAME
         self.data = pd.DataFrame({
-            'A': [1, 2, np.nan, 4],
+            'A': [1, 2, np.nan, 3],
             'B': [np.nan, 5, 6, 7],
             'C': ['cat', 'dog', 'mouse', None]
         })
-        # Expected Result after 'mean' imputation
+        # EXPECTED RESULTS
         self.expected_imputed_mean = pd.DataFrame({
-            'A': [1, 2, 2.3333333333333335, 4],  # mean of [1, 2, 4]
+            'A': [1, 2, 2, 3],  # mean of [1, 2, 4]
             'B': [6.0, 5.0, 6.0, 7.0],  # mean of [5, 6, 7]
-            'C': ['cat', 'dog', 'mouse', None]  # untouched
+            'C': ['cat', 'dog', 'mouse', None]  # should remain untouched
         })
 
     def test_imputer_fit_transform(self):
@@ -26,34 +26,33 @@ class TestImputer(unittest.TestCase):
         imputer.fit(self.data)
         transformed_data = imputer.transform(self.data.copy())
         
-        # Test that numeric columns are imputed correctly
+        # TESTING MEANS ARE CORRECT
         pd.testing.assert_frame_equal(
             transformed_data[['A', 'B']], 
             self.expected_imputed_mean[['A', 'B']],
             check_exact=False  # Allow for floating-point precision differences
         )
         
-        # Test that non-numeric columns remain unchanged
+        # TESTING THAT NON-NUMERIC COLUMNS REMAIN THE SAME
         self.assertTrue(
             all(transformed_data['C'] == self.data['C']),
             "Non-numeric columns should remain unchanged."
         )
 
     def test_imputer_fit_all_numeric(self):
-        # Test the Imputer with automatic selection of numeric columns
+        # TESTING THAT THE IMPUTER SELECTS THE COLUMNS CORRECTLY
         imputer = Imputer(strategy='mean')
         
         imputer.fit(self.data)
         transformed_data = imputer.transform(self.data.copy())
         
-        # Test numeric columns are imputed correctly
+        # RESULTS SHOULD BE THE SAME AS BEFORE
         pd.testing.assert_frame_equal(
             transformed_data[['A', 'B']], 
             self.expected_imputed_mean[['A', 'B']],
             check_exact=False
         )
         
-        # Test that non-numeric columns remain unchanged
         self.assertTrue(
             all(transformed_data['C'] == self.data['C']),
             "Non-numeric columns should remain unchanged."
